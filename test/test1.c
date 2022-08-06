70ddf22bef73372011e1fe6fb8adf133a2f4bd3b
RhodaAdigun <106974772+RhodaAdigun@users.noreply.github.com>
2022-08-06 15:39:24 +0100

    test printf

--- test/test1.c
+++ test/test1.c
@@ -8,8 +8,16 @@
 
 int main(void)
 {
-	int y;
-
+	int y,len,len2;
+	
+	len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+	 _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+	printf("\n------newline -------\n");
+	
 	y = _printf("Hello %c %s world\n", 'P', "Ife");
 	printf("n = %d\n", y);
 	return (0);
