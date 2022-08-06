2d22f12342d6403e45751c8215b74284752aecda
RhodaAdigun <ifeoluwa.adigun2102@gmail.com>
2022-08-06 14:06:04 +0000

    Creates a printf function

---
+++ test/test1.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - use to test code
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y;
+
+	y = _printf("Hello %c %s world\n", 'P', "Ife");
+	printf("n = %d\n", y);
+	return (0);
+}
