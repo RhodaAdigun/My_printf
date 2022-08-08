4d0d3580446bcab9d212ae46043cfabb0a51d39c
RhodaAdigun <106974772+RhodaAdigun@users.noreply.github.com>
2022-08-08 21:29:27 +0100

    Header file

--- main.h
+++ main.h
@@ -12,4 +12,15 @@ int _printf(const char *format, ...);
 int print_string(const char *s);
 int _putchar(char c);
 
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*pointer)(va_list);
+} specifier;
+
 #endif
