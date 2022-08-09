f6a6d917f8bd9a2ae79c3e30b72791427ea939a5
RhodaAdigun <106974772+RhodaAdigun@users.noreply.github.com>
2022-08-09 18:32:45 +0100

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -19,3 +19,33 @@ int print_string(const char *s)
 
 	return (i);
 }
+
+/**
+ * print_string - Print out a string
+ * @s: Pointer to the input string
+ * Return: int
+ */
+int print_STRING(const char *s)
+{
+	int i, num;
+
+	num = 0;
+	if (s)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			if (s[i] <= 32 || s[i] >= 127)
+			{
+				_putchar('\\');
+				_putchar('x');
+				_putchar('0');
+				_printf("%X", s[i]);
+				num += 4;
+				continue;
+			}
+			_putchar(s[i]);
+			num++;
+		}
+	}
+	return (num);
+}
